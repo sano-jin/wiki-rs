46f89635e9442a1816e5b7f1b9e9ab38c9d60330
sano <sano@ueda.info.waseda.ac.jp>
2022-04-27 16:25:14 +0900

    updated

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,108 @@
-use std::fs::File;
 use std::io;
-use std::io::prelude::*;
 
 use actix_files;
-use actix_web::{middleware, web, App, Error, HttpResponse, HttpServer};
+use actix_web::{middleware, web, App, Error, HttpRequest, HttpResponse, HttpServer};
 use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
 use serde::{Deserialize, Serialize};
-
+use std::fs::File;
+use std::io::prelude::*;
 use std::path::{Path, PathBuf};
-extern crate pulldown_cmark;
+use urlencoding;
+// Newly added pulldown_cmark
 use pulldown_cmark::{html, Options, Parser};
 
-use urlencoding;
+/// Get the new path <root_dir>/<encoded filename>
+fn get_path(root_dir: &str, filename: &str) -> PathBuf {
+    let encoded = urlencoding::encode(&filename); // encode the filename
+    Path::new(&root_dir).join(Path::new(&encoded.into_owned()))
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct NewPageObj {
+    path: String,
+    body: String,
+}
+
+/// Create and Update the file with POST method
+async fn post(item: web::Json<NewPageObj>) -> Result<HttpResponse, Error> {
+    println!("post {:?}", item);
+
+    // Update the file with the given contents
+    let path: PathBuf = get_path("public/edit", &item.path);
+    println!("writing to the file {:?}", path);
+    let mut file = File::create(&path)?;
+    file.write_all(item.body.as_bytes())?;
+
+    // Set parser options
+    let mut options = Options::empty();
+    options.insert(Options::ENABLE_STRIKETHROUGH);
+    options.insert(Options::ENABLE_TABLES);
+    options.insert(Options::ENABLE_FOOTNOTES);
+    options.insert(Options::ENABLE_TASKLISTS);
+    options.insert(Options::ENABLE_SMART_PUNCTUATION);
+    options.insert(Options::ENABLE_HEADING_ATTRIBUTES);
+
+    // Parse the given markdown with the pulldown_cmark parser
+    println!("parsing the given markdown with the pulldown_cmark parser");
+    let parser = Parser::new(&item.body);
+    let mut html_buf = String::new();
+    html::push_html(&mut html_buf, parser);
+    println!("parsed: {}", html_buf);
+
+    // decode the path to obtain the title
+    let title = urlencoding::decode(&item.path).expect("cannot decode");
+
+    // Open the default file
+    let default_page = std::fs::read_to_string("public/layouts/page.html")?;
+    // Replace the title, path, contents
+    let page = default_page
+        .replace("TITLE", &title.into_owned())
+        .replace("PATH", &item.path)
+        .replace("BODY", &html_buf);
+
+    // Update the file with the given contents
+    let path: PathBuf = get_path("public/pages", &item.path);
+    println!("writing to the file {:?}", path);
+    let mut file = File::create(&path)?;
+    file.write_all(page.as_bytes())?;
+
+    // TODO: navigate to the new page created
+    let url = format!("/pages?path={}", &item.path);
+    Ok(HttpResponse::Ok().json(url))
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct QueryPath {
+    path: String,
+}
+
+/// Delete the file with DELETE method
+async fn delete(item: web::Query<QueryPath>) -> Result<HttpResponse, Error> {
+    println!("delete ? {:?}", item);
+
+    // delete the markdown file
+    let path: PathBuf = get_path("public/edit", &item.path);
+    std::fs::remove_file(&path)?;
+
+    // delete the html file
+    let path: PathBuf = get_path("public/pages", &item.path);
+    std::fs::remove_file(&path)?;
+
+    // TODO: navigate to the root page
+    Ok(HttpResponse::Ok().json("deleted"))
+}
+
+/// GET the page
+async fn get_page(item: web::Query<QueryPath>) -> Result<HttpResponse, Error> {
+    println!("get_page ? {:?}", item);
+
+    // Load the file
+    let path = get_path("public/pages", &item.path);
+    let contents = std::fs::read_to_string(&path)?;
+
+    // Return the response and display the html file on the browser
+    Ok(HttpResponse::Ok().content_type("text/html").body(contents))
+}
 
 /// Read a file
 /// If the file doesn not exists, then return the default string
@@ -26,193 +117,32 @@ fn read_with_default(path: &str, default: &str) -> String {
     }
 }
 
-/// Get the page at the fiven path
-/// Add recent updated file names list
-fn get_at(path: PathBuf) -> Result<HttpResponse, Error> {
-    println!("path: {:?}", path);
-
-    // Load the file
-    let contents = std::fs::read_to_string(&path)?;
-
-    // Add recent updated file names list
-    let index_ul = read_with_default("public/index_ul", "");
-
-    println!("index_ul: {}", index_ul);
-    let contents = contents.replace("INDEX_UL", &index_ul);
-
-    Ok(HttpResponse::Ok().content_type("text/html").body(contents))
-}
-
-/// The top page
-async fn index() -> Result<HttpResponse, Error> {
-    get_at(PathBuf::from("public/index.html"))
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct QueryPath {
-    path: String,
-}
-
-/// Get the new path <root>/<encoded path>
-fn get_path(root: &str, path: &str) -> PathBuf {
-    let encoded = urlencoding::encode(&path);
-    Path::new(&root).join(Path::new(&encoded.into_owned()))
-}
-
-/// GET the page
-async fn get_page(item: web::Query<QueryPath>) -> Result<HttpResponse, Error> {
-    println!("get_page ? {:?}", item);
-    get_at(get_path("public/pages", &item.path))
-}
-
 /// This handler uses json extractor with limit
 /// GET the page for editing the page
-async fn get_edit_page(item: web::Query<QueryPath>) -> Result<HttpResponse, Error> {
+async fn get_editor(item: web::Query<QueryPath>) -> Result<HttpResponse, Error> {
     println!("get_edit_page ? {:?}", item);
-
-    // TODO: check the path is valid
     let path: PathBuf = get_path("public/edit", &item.path);
-    println!("path: {:?}", path);
-
-    // TODO: use BufReader (low priority)
-    // if the file does not exists (that is, user is trying to create a new page),
-    // return the default string (currently empty string)
     let contents = read_with_default(&path.to_string_lossy(), "");
 
     // decode the path to obtain the title
     let title = urlencoding::decode(&item.path).expect("cannot decode");
 
     // Open the file for editing
-    let edit_page = std::fs::read_to_string("static/edit.html")?;
-    let edit_page = edit_page
+    let editor = std::fs::read_to_string("public/layouts/edit.html")?;
+    // Replace the contents
+    let editor = editor
         .replace("TITLE", &title.into_owned())
-        .replace("PATH", &item.path)
         .replace("MARKDOWN", &contents);
 
-    Ok(HttpResponse::Ok().content_type("text/html").body(edit_page))
+    Ok(HttpResponse::Ok().content_type("text/html").body(editor))
 }
 
-/// Update the file for the html list of the file names
-fn update_index_ul(index: Vec<&str>) -> Result<(), Error> {
-    // generate the html list of the files
-    let index_ul: Vec<String> = index
-        .into_iter()
-        .map(|filename| {
-            format!(
-                "<li><a href=\"/pages?path={}\">{}</a></li>",
-                urlencoding::encode(&filename),
-                filename
-            )
-        })
-        .collect();
-    let index_ul_str = index_ul.join("\n");
-
-    let mut file = File::create("public/index_ul")?;
-    file.write_all(&index_ul_str.as_bytes())?;
-    Ok(())
-}
-
-/// write `contents` to the file `root_dir/path`
-fn update_file(root_dir: &str, filename: &str, contents: &str) -> Result<(), Error> {
-    let path: PathBuf = get_path(&root_dir, &filename);
-    println!("updating the file at {:?}", path);
-
-    // Write to a file with the given contents
-    let mut file = File::create(&path)?;
-    file.write_all(&contents.as_bytes())?;
-
-    // Update index
-    let index_file = read_with_default("public/index", "");
-    let mut index: Vec<&str> = index_file.lines().collect();
-
-    index.retain(|value| *value != filename); // remove if the filename already exists on the index
-    index.insert(0, &filename); // push front the filename as the name of the most recent updated file
-    let index_str = index.join("\n");
-    println!("new index: {:?}", index);
-
-    // update the index file
-    let mut file = File::create("public/index")?;
-    file.write_all(&index_str.as_bytes())?;
-
-    // generate the html list of the files
-    update_index_ul(index)?;
-
-    return Ok(());
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct NewPageObj {
-    path: String,
-    body: String,
-}
-
-/// This handler uses json extractor with limit
-/// Post the edited file to update the page
-async fn post_edited(item: web::Json<NewPageObj>) -> Result<HttpResponse, Error> {
-    println!("post_edited ? {:?}", item);
-
-    // Update the markdown file with the given contents
-    update_file("public/edit", &item.path, &item.body)?;
-
-    // Set parser options
-    let mut options = Options::empty();
-    options.insert(Options::ENABLE_STRIKETHROUGH);
-    options.insert(Options::ENABLE_TABLES);
-    options.insert(Options::ENABLE_FOOTNOTES);
-    options.insert(Options::ENABLE_TASKLISTS);
-    options.insert(Options::ENABLE_SMART_PUNCTUATION);
-    options.insert(Options::ENABLE_HEADING_ATTRIBUTES);
-
-    // Parse the given markdown with the pulldown_cmark parser
-    let parser = Parser::new_ext(&item.body, options);
-    let mut html_buf = String::new();
-    html::push_html(&mut html_buf, parser);
-    println!("parsed html: {:?}", html_buf);
-
-    // decode the path to obtain the title
-    let title = urlencoding::decode(&item.path).expect("cannot decode");
-
-    // Open the default file
-    let default_page = std::fs::read_to_string("static/page.html")?;
-    let default_page = default_page
-        .replace("TITLE", &title.into_owned())
-        .replace("PATH", &item.path)
-        .replace("BODY", &html_buf);
-
-    update_file("public/pages", &item.path, &default_page)?;
-
-    // TODO: navigate to the new page created
-    let url = format!("/pages?path={}", &item.path);
-    Ok(HttpResponse::Ok().json(url))
-}
-
-/// This handler uses json extractor with limit
-async fn delete_page(item: web::Query<QueryPath>) -> Result<HttpResponse, Error> {
-    println!("delete_page ? {:?}", item);
-
-    // Remove the markdown file
-    std::fs::remove_file(get_path("public/edit", &item.path))?;
-
-    // Remove the html file
-    std::fs::remove_file(get_path("public/pages", &item.path))?;
-
-    // Update index
-    let index_file = read_with_default("public/index", "");
-    let mut index: Vec<&str> = index_file.lines().collect();
-
-    index.retain(|value| *value != &item.path); // remove if the filename already exists on the index
-    let index_str = index.join("\n");
-    println!("new index: {:?}", index);
-
-    // update the index file
-    let mut file = File::create("public/index")?;
-    file.write_all(&index_str.as_bytes())?;
-
-    // generate the html list of the files
-    update_index_ul(index)?;
-
-    // TODO: navigate to the root page
-    Ok(HttpResponse::Ok().json("deleted")) // <- send json response
+/// simple handle
+async fn index(req: HttpRequest) -> Result<HttpResponse, Error> {
+    println!("{:?}", req);
+    Ok(HttpResponse::Ok()
+        .content_type("text/plain")
+        .body("Welcome!"))
 }
 
 #[actix_web::main]
@@ -234,19 +164,18 @@ async fn main() -> io::Result<()> {
             // enable logger
             .wrap(middleware::Logger::default())
             // with path parameters
-            // Viewing the pages
-            .service(actix_files::Files::new("/assets", "public/assets").show_files_listing())
-            // Editing
-            .service(web::resource("/pages").route(web::get().to(get_page)))
+            // **Newly added here**
+            .service(
+                web::resource("/pages").route(web::get().to(get_page)), // GET the page
+            )
             .service(
                 web::resource("/edit")
-                    // GET the page for editing
-                    .route(web::get().to(get_edit_page))
-                    // POST the new markdown
-                    .route(web::post().to(post_edited))
-                    // DELETE the page
-                    .route(web::delete().to(delete_page)),
+                    .route(web::get().to(get_editor)) // GET the editor
+                    .route(web::post().to(post)) // POST the new contents to update the file
+                    .route(web::delete().to(delete)), // Delete the file
             )
+            // GET /files/**/*.html and return the file /public/**/*.html
+            .service(actix_files::Files::new("/assets", "public/assets").show_files_listing())
             // register simple handler, handle all methods
             .service(web::resource("/index.html").to(index))
             // with path parameters
