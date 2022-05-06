34e8803e913fc789e2a33113b64795725be3d19c
sano <sano@ueda.info.waseda.ac.jp>
2022-05-06 13:52:48 +0900

    コメントアウトした機能を削除

--- src/models/pages.rs
+++ src/models/pages.rs
@@ -6,7 +6,6 @@ use std::fs::File;
 use std::io::prelude::*;
 use urlencoding;
 
-// use either::*;
 use regex::Regex;
 
 #[derive(Debug)]
@@ -59,6 +58,7 @@ impl Page {
         // and image by its alt text.
         let parser = Parser::new_ext(&markdown, Options::empty()).map(|event| match event {
             // Event::Text(text) => Event::Text(text.replace("Peter", "John").into()),
+            // 画像などのファイルのパスをいじりたい
             Event::Start(Tag::Image(LinkType::Inline, url, title))
             | Event::End(Tag::Image(LinkType::Inline, url, title)) => {
                 println!("url is {}", url);
@@ -72,35 +72,6 @@ impl Page {
             }
             _ => event,
         });
-        // let parser = parser.flat_map(|event| match event {
-        //     // Event::Text(text) => Event::Text(text.replace("Peter", "John").into()),
-        //     Event::Text(text) => {
-        //         println!("text is {}", text);
-        //         let re = Regex::new(r"(^https?://[^\s]*)|^/").unwrap();
-        //         let text_str = text.to_string();
-        //         // let mut url = url;
-        //         if re.is_match(&text_str) {
-        //             println!("{}", text_str);
-        //             return Left(
-        //                 std::iter::once(Event::Start(Tag::Link(
-        //                     LinkType::Inline,
-        //                     CowStr::from(text_str.to_owned()),
-        //                     CowStr::from(text_str.to_owned()),
-        //                 )))
-        //                 .chain(std::iter::once(Event::End(Tag::Link(
-        //                     LinkType::Inline,
-        //                     CowStr::from(text_str.to_owned()),
-        //                     CowStr::from(text_str.to_owned()),
-        //                 )))),
-        //             );
-        //             // return std::iter::once(Event::Text(text));
-        //         }
-        //         Right(std::iter::once(Event::Text(text)))
-        //         // vec![Event::Text(text)].iter()
-        //     }
-        //     // _ => vec![event].iter(),
-        //     _ => Right(std::iter::once(event)),
-        // });
 
         let mut html_buf = String::new();
         html::push_html(&mut html_buf, parser);
